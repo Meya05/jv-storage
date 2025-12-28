213286701cc5ad74292f09b0a540da0b25c77682
Meya05 <majka.dunin_05@wp.pl>
2025-12-28 17:11:23 +0100

    jv-storage: poprawne rozwiazanie 2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
@@ -13,7 +14,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i].equals(key)) {
+            if (Objects.equals(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -27,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @SuppressWarnings("unchecked")
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i].equals(key)) {
+            if (Objects.equals(keys[i], key)) {
                 return (V) values[i];
             }
         }
